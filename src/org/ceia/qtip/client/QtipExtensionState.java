4d24e7c181a3ed4da24def2a94f78f6ae77883e1
Guglielmo Moretti <guglielmo.moretti@gmail.com>
2013-04-29 21:31:03 +0200

    Delete Vaadin related dependency

--- src/org/ceia/qtip/client/QtipExtensionState.java
+++
@@ -1,7 +0,0 @@
-package org.ceia.qtip.client;
-
-public class QtipExtensionState extends com.vaadin.shared.AbstractComponentState {
-
-    private static final long serialVersionUID = -5902332489332191551L;
-
-}
